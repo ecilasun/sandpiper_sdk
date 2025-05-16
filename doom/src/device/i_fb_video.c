f0cef4e00ba826dedb20de66af3b78089639fc45
Engin Cilasun <ecilasun@me.com>
2025-05-16 22:26:33 +0000

    fix color output error in doom

--- doom/src/device/i_fb_video.c
+++ doom/src/device/i_fb_video.c
@@ -43,6 +43,8 @@ void sigint_handler(int s)
 
 void I_InitGraphics (void)
 {
+	usegamma = 1;
+
 	SPInitPlatform(&s_platform);
 
 	VPUInitVideo(&s_vctx, &s_platform);
@@ -56,10 +58,12 @@ void I_InitGraphics (void)
 	atexit(shutdowncleanup);
 	signal(SIGINT, &sigint_handler);
 
-	VPUSetWriteAddress(&s_vctx, (uint32_t)frameBufferA.cpuAddress);
-	VPUSetScanoutAddress(&s_vctx, (uint32_t)frameBufferB.dmaAddress);
 	VPUSetDefaultPalette(&s_vctx);
 	VPUSetVideoMode(&s_vctx, VIDEO_MODE, VIDEO_COLOR, EVS_Enable);
+
+	printf("mode:320x240x8bpp stride: %d\n", stride);
+	printf("A: 0x%08X 0x%08X\n", (uint32_t)frameBufferA.cpuAddress, (uint32_t)frameBufferA.dmaAddress);
+	printf("B: 0x%08X 0x%08X\n", (uint32_t)frameBufferB.cpuAddress, (uint32_t)frameBufferB.dmaAddress);
 
 	s_sctx.cycle = 0;
 	s_sctx.framebufferA = &frameBufferA;
@@ -81,10 +85,15 @@ void I_StartFrame (void)
 // Takes full 8 bit values.
 void I_SetPalette (byte* palette)
 {
-    byte c;
-    // set the X colormap entries
-    for (int i=0 ; i<256 ; i++)
-		VPUSetPal(&s_vctx, i, palette[0], palette[1], palette[2]);
+	byte r, g, b;
+	// set the X colormap entries
+	for (int i=0 ; i<256 ; i++)
+	{
+		r = gammatable[usegamma][*palette++]>>4;
+		g = gammatable[usegamma][*palette++]>>4;
+		b = gammatable[usegamma][*palette++]>>4;
+		VPUSetPal(&s_vctx, i, r, g, b);
+	}
 }
 
 void I_UpdateNoBlit (void)
@@ -102,10 +111,12 @@ void I_FinishUpdate (void)
 	uint32_t stride = VPUGetStride(VIDEO_MODE, VIDEO_COLOR);
 
 	uint8_t *vramBase = (uint8_t*)s_vctx.m_cpuWriteAddressCacheAligned;
-	for (uint32_t y=0; y<VIDEO_HEIGHT; y++)
+	uint8_t *scr = screens[0];
+	for (uint32_t y=0; y<SCREENHEIGHT; y++)
 	{
-		memcpy(vramBase, screens[0]+y*SCREENWIDTH, SCREENWIDTH);
+		memcpy(vramBase, scr, SCREENWIDTH);
 		vramBase += stride;
+		scr += SCREENWIDTH;
 	}
 
 	VPUSwapPages(&s_vctx, &s_sctx);
