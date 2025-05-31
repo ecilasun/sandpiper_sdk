a33d63ba20250125acd7e3fbbea8189269de865f
Engin Cilasun <ecilasun@me.com>
2025-05-31 18:20:23 +0000

    updates

--- drivers/framebuffer/files/sandyfb.c
+++ drivers/framebuffer/files/sandyfb.c
@@ -1,485 +1,118 @@
+#include <linux/init.h>
 #include <linux/module.h>
 #include <linux/kernel.h>
-#include <linux/errno.h>
-#include <linux/string.h>
-#include <linux/mm.h>
-#include <linux/vmalloc.h>
-#include <linux/delay.h>
-#include <linux/interrupt.h>
-#include <linux/platform_device.h>
-
 #include <linux/fb.h>
-#include <linux/init.h>
+#include <linux/mm.h>
 
-// Sufficient space for two 640x480 frame buffers
-#define VIDEOMEMSIZE	(2*640*480)
+#define WIDTH 640
+#define HEIGHT 480
 
-static void *videomemory;
-static u_long videomemorysize = VIDEOMEMSIZE;
-module_param(videomemorysize, ulong, 0);
-MODULE_PARM_DESC(videomemorysize, "RAM available to frame buffer (in bytes)");
+static const uint16_t vgapalette[] __attribute__((aligned(16))) = {
+0x0000, 0x000A, 0x00A0, 0x00AA, 0x0A00, 0x0A0A, 0x0A50, 0x0AAA,
+0x0555, 0x055F, 0x05F5, 0x05FF, 0x0F55, 0x0F5F, 0x0FF5, 0x0FFF,
+0x0000, 0x0111, 0x0222, 0x0333, 0x0444, 0x0555, 0x0666, 0x0777,
+0x0888, 0x0999, 0x0AAA, 0x0BBB, 0x0CCC, 0x0DDD, 0x0EEE, 0x0FFF,
+0x000F, 0x040F, 0x080F, 0x0B0F, 0x0F0F, 0x0F0B, 0x0F08, 0x0F04,
+0x0F00, 0x0F40, 0x0F80, 0x0FB0, 0x0FF0, 0x0BF0, 0x08F0, 0x04F0,
+0x00F0, 0x00F4, 0x00F8, 0x00FB, 0x00FF, 0x00BF, 0x008F, 0x004F,
+0x088F, 0x098F, 0x0B8F, 0x0D8F, 0x0F8F, 0x0F8D, 0x0F8B, 0x0F89,
+0x0F88, 0x0F98, 0x0FB8, 0x0FD8, 0x0FF8, 0x0DF8, 0x0BF8, 0x09F8,
+0x08F8, 0x08F9, 0x08FB, 0x08FD, 0x08FF, 0x08DF, 0x08BF, 0x089F,
+0x0BBF, 0x0CBF, 0x0DBF, 0x0EBF, 0x0FBF, 0x0FBE, 0x0FBD, 0x0FBC,
+0x0FBB, 0x0FCB, 0x0FDB, 0x0FEB, 0x0FFB, 0x0EFB, 0x0DFB, 0x0CFB,
+0x0BFB, 0x0BFC, 0x0BFD, 0x0BFE, 0x0BFF, 0x0BEF, 0x0BDF, 0x0BCF,
+0x0007, 0x0107, 0x0307, 0x0507, 0x0707, 0x0705, 0x0703, 0x0701,
+0x0700, 0x0710, 0x0730, 0x0750, 0x0770, 0x0570, 0x0370, 0x0170,
+0x0070, 0x0071, 0x0073, 0x0075, 0x0077, 0x0057, 0x0037, 0x0017,
+0x0337, 0x0437, 0x0537, 0x0637, 0x0737, 0x0736, 0x0735, 0x0734,
+0x0733, 0x0743, 0x0753, 0x0763, 0x0773, 0x0673, 0x0573, 0x0473,
+0x0373, 0x0374, 0x0375, 0x0376, 0x0377, 0x0367, 0x0357, 0x0347,
+0x0557, 0x0557, 0x0657, 0x0657, 0x0757, 0x0756, 0x0756, 0x0755,
+0x0755, 0x0755, 0x0765, 0x0765, 0x0775, 0x0675, 0x0675, 0x0575,
+0x0575, 0x0575, 0x0576, 0x0576, 0x0577, 0x0567, 0x0567, 0x0557,
+0x0004, 0x0104, 0x0204, 0x0304, 0x0404, 0x0403, 0x0402, 0x0401,
+0x0400, 0x0410, 0x0420, 0x0430, 0x0440, 0x0340, 0x0240, 0x0140,
+0x0040, 0x0041, 0x0042, 0x0043, 0x0044, 0x0034, 0x0024, 0x0014,
+0x0224, 0x0224, 0x0324, 0x0324, 0x0424, 0x0423, 0x0423, 0x0422,
+0x0422, 0x0422, 0x0432, 0x0432, 0x0442, 0x0342, 0x0342, 0x0242,
+0x0242, 0x0242, 0x0243, 0x0243, 0x0244, 0x0234, 0x0234, 0x0224,
+0x0224, 0x0324, 0x0324, 0x0324, 0x0424, 0x0423, 0x0423, 0x0423,
+0x0422, 0x0432, 0x0432, 0x0432, 0x0442, 0x0342, 0x0342, 0x0342,
+0x0242, 0x0243, 0x0243, 0x0243, 0x0244, 0x0234, 0x0234, 0x0234,
+0x0000, 0x0000, 0x0000, 0x0000, 0x0000, 0x0000, 0x0000, 0x0000 };
 
-static char *mode_option = NULL;
-module_param(mode_option, charp, 0);
-MODULE_PARM_DESC(mode_option, "Preferred video mode (e.g. 640x480-8@60)");
+static void op_fillrect(struct fb_info * info, const struct fb_fillrect * rect) {
+    printk(KERN_ALERT "fillrect\n");
+}
 
-static const struct fb_videomode vfb_default = {
-	.xres =		640,
-	.yres =		480,
-	.pixclock =	20000,
-	.left_margin =	64,
-	.right_margin =	64,
-	.upper_margin =	32,
-	.lower_margin =	32,
-	.hsync_len =	64,
-	.vsync_len =	2,
-	.vmode =	FB_VMODE_NONINTERLACED,
+static void op_copyarea(struct fb_info * info, const struct fb_copyarea * area) {
+    printk(KERN_ALERT "copyarea\n");
+}
+
+static void op_imageblit(struct fb_info * info, const struct fb_image * image) {
+    printk(KERN_ALERT "imageblit\n");
+}
+
+static struct fb_info * info;
+
+static struct fb_ops ops = {
+    .owner = THIS_MODULE,
+    //.fb_open = sandpiperfb_open,
+    //.fb_release = sandpiperfb_release,
+    .fb_fillrect = op_fillrect,
+    .fb_copyarea = op_copyarea,
+    .fb_imageblit = op_imageblit,
 };
 
-static struct fb_fix_screeninfo vfb_fix = {
-	.id =		"sandpiper FB",
-	.type =		FB_TYPE_PACKED_PIXELS,
-	.visual =	FB_VISUAL_PSEUDOCOLOR,
-	.xpanstep =	1,
-	.ypanstep =	1,
-	.ywrapstep =	1,
-	.accel =	FB_ACCEL_NONE,
+static const struct fb_fix_screeninfo fix = {
+    .id = "sandpiperfb",
+    .smem_start = 0x18000000, // Top of reserved memory, physical address
+    .smem_len = 640 * 480, // 640x480x8bpp
+    .type = FB_TYPE_PACKED_PIXELS,
+    .visual = FB_VISUAL_PSEUDOCOLOR,
+    .line_length = 640, // Stride is a multiple of 128
 };
 
-static bool vfb_enable __initdata = 0;	/* disabled by default */
-module_param(vfb_enable, bool, 0);
-MODULE_PARM_DESC(vfb_enable, "Enable sandpiper FB driver");
-
-static int vfb_check_var(struct fb_var_screeninfo *var,
-			 struct fb_info *info);
-static int vfb_set_par(struct fb_info *info);
-static int vfb_setcolreg(u_int regno, u_int red, u_int green, u_int blue,
-			 u_int transp, struct fb_info *info);
-static int vfb_pan_display(struct fb_var_screeninfo *var,
-			   struct fb_info *info);
-static int vfb_mmap(struct fb_info *info,
-		    struct vm_area_struct *vma);
-
-static const struct fb_ops vfb_ops = {
-	.owner		= THIS_MODULE,
-	__FB_DEFAULT_SYSMEM_OPS_RDWR,
-	.fb_check_var	= vfb_check_var,
-	.fb_set_par	= vfb_set_par,
-	.fb_setcolreg	= vfb_setcolreg,
-	.fb_pan_display	= vfb_pan_display,
-	__FB_DEFAULT_SYSMEM_OPS_DRAW,
-	.fb_mmap	= vfb_mmap,
+static const struct fb_var_screeninfo var = {
+    .xres = WIDTH,
+    .yres = HEIGHT,
+    .xres_virtual = WIDTH,
+    .yres_virtual = HEIGHT,
+    .bits_per_pixel = 8,
+    .height = HEIGHT,
+    .width = WIDTH,
+    .vmode = FB_VMODE_NONINTERLACED,
+    .red = { .length = 8, .offset = 0, },
+    .green = { .length = 8, .offset = 0, },
+    .blue = { .length = 8, .offset = 0, }
 };
 
-static u_long get_stride(int xres_virtual, int bpp)
+static int mod_init(void)
 {
-	return 640; // Always 640 in this mode
+    int err;
+    info = framebuffer_alloc(0, 0);
+    if (!info)
+		return -1;
+    info->fbops = &ops;
+    info->fix = fix;
+	info->pseudo_palette = vgapalette;
+    info->var = var;
+    info->screen_base = ioremap(fix.smem_start, 640 * 480);
+    err = fb_alloc_cmap(&info->cmap, 256, 0);
+    if (err < 0) return -2;
+    err = register_framebuffer(info);
+    if (err < 0) return -3;
+    return 0;
 }
 
-    /*
-     *  Setting the video mode has been split into two parts.
-     *  First part, xxxfb_check_var, must not write anything
-     *  to hardware, it should only verify and adjust var.
-     *  This means it doesn't alter par but it does use hardware
-     *  data from it to check this var.
-     */
-
-static int vfb_check_var(struct fb_var_screeninfo *var,
-			 struct fb_info *info)
+static void mod_exit(void)
 {
-	u_long line_length;
-
-	/*
-	 *  FB_VMODE_CONUPDATE and FB_VMODE_SMOOTH_XPAN are equal!
-	 *  as FB_VMODE_SMOOTH_XPAN is only used internally
-	 */
-
-	if (var->vmode & FB_VMODE_CONUPDATE) {
-		var->vmode |= FB_VMODE_YWRAP;
-		var->xoffset = info->var.xoffset;
-		var->yoffset = info->var.yoffset;
-	}
-
-	/*
-	 *  Some very basic checks
-	 */
-	if (!var->xres)
-		var->xres = 1;
-	if (!var->yres)
-		var->yres = 1;
-	if (var->xres > var->xres_virtual)
-		var->xres_virtual = var->xres;
-	if (var->yres > var->yres_virtual)
-		var->yres_virtual = var->yres;
-	if (var->bits_per_pixel <= 1)
-		var->bits_per_pixel = 1;
-	else if (var->bits_per_pixel <= 8)
-		var->bits_per_pixel = 8;
-	else if (var->bits_per_pixel <= 16)
-		var->bits_per_pixel = 16;
-	else if (var->bits_per_pixel <= 24)
-		var->bits_per_pixel = 24;
-	else if (var->bits_per_pixel <= 32)
-		var->bits_per_pixel = 32;
-	else
-		return -EINVAL;
-
-	if (var->xres_virtual < var->xoffset + var->xres)
-		var->xres_virtual = var->xoffset + var->xres;
-	if (var->yres_virtual < var->yoffset + var->yres)
-		var->yres_virtual = var->yoffset + var->yres;
-
-	/*
-	 *  Memory limit
-	 */
-	line_length = get_stride(var->xres_virtual, var->bits_per_pixel);
-	if (line_length * var->yres_virtual > videomemorysize)
-		return -ENOMEM;
-
-	/*
-	 * Now that we checked it we alter var. The reason being is that the video
-	 * mode passed in might not work but slight changes to it might make it
-	 * work. This way we let the user know what is acceptable.
-	 */
-	switch (var->bits_per_pixel) {
-	case 1:
-	case 8:
-		var->red.offset = 0;
-		var->red.length = 8;
-		var->green.offset = 0;
-		var->green.length = 8;
-		var->blue.offset = 0;
-		var->blue.length = 8;
-		var->transp.offset = 0;
-		var->transp.length = 0;
-		break;
-	case 16:		/* RGBA 5551 */
-		if (var->transp.length) {
-			var->red.offset = 0;
-			var->red.length = 5;
-			var->green.offset = 5;
-			var->green.length = 5;
-			var->blue.offset = 10;
-			var->blue.length = 5;
-			var->transp.offset = 15;
-			var->transp.length = 1;
-		} else {	/* RGB 565 */
-			var->red.offset = 0;
-			var->red.length = 5;
-			var->green.offset = 5;
-			var->green.length = 6;
-			var->blue.offset = 11;
-			var->blue.length = 5;
-			var->transp.offset = 0;
-			var->transp.length = 0;
-		}
-		break;
-	case 24:		/* RGB 888 */
-		var->red.offset = 0;
-		var->red.length = 8;
-		var->green.offset = 8;
-		var->green.length = 8;
-		var->blue.offset = 16;
-		var->blue.length = 8;
-		var->transp.offset = 0;
-		var->transp.length = 0;
-		break;
-	case 32:		/* RGBA 8888 */
-		var->red.offset = 0;
-		var->red.length = 8;
-		var->green.offset = 8;
-		var->green.length = 8;
-		var->blue.offset = 16;
-		var->blue.length = 8;
-		var->transp.offset = 24;
-		var->transp.length = 8;
-		break;
-	}
-	var->red.msb_right = 0;
-	var->green.msb_right = 0;
-	var->blue.msb_right = 0;
-	var->transp.msb_right = 0;
-
-	return 0;
+    return;
 }
 
-/* This routine actually sets the video mode. It's in here where we
- * the hardware state info->par and fix which can be affected by the
- * change in par. For this driver it doesn't do much.
- */
-static int vfb_set_par(struct fb_info *info)
-{
-	switch (info->var.bits_per_pixel) {
-	case 1:
-		info->fix.visual = FB_VISUAL_MONO01;
-		break;
-	case 8:
-		info->fix.visual = FB_VISUAL_PSEUDOCOLOR;
-		break;
-	case 16:
-	case 24:
-	case 32:
-		info->fix.visual = FB_VISUAL_TRUECOLOR;
-		break;
-	}
+module_init(mod_init);
+module_exit(mod_exit);
 
-	info->fix.line_length = get_stride(info->var.xres_virtual, info->var.bits_per_pixel);
-
-	return 0;
-}
-
-    /*
-     *  Set a single color register. The values supplied are already
-     *  rounded down to the hardware's capabilities (according to the
-     *  entries in the var structure). Return != 0 for invalid regno.
-     */
-
-static int vfb_setcolreg(u_int regno, u_int red, u_int green, u_int blue,
-			 u_int transp, struct fb_info *info)
-{
-	if (regno >= 256)	/* no. of hw registers */
-		return 1;
-	/*
-	 * Program hardware... do anything you want with transp
-	 */
-
-	/* grayscale works only partially under directcolor */
-	if (info->var.grayscale) {
-		/* grayscale = 0.30*R + 0.59*G + 0.11*B */
-		red = green = blue =
-		    (red * 77 + green * 151 + blue * 28) >> 8;
-	}
-
-	/* Directcolor:
-	 *   var->{color}.offset contains start of bitfield
-	 *   var->{color}.length contains length of bitfield
-	 *   {hardwarespecific} contains width of RAMDAC
-	 *   cmap[X] is programmed to (X << red.offset) | (X << green.offset) | (X << blue.offset)
-	 *   RAMDAC[X] is programmed to (red, green, blue)
-	 *
-	 * Pseudocolor:
-	 *    var->{color}.offset is 0 unless the palette index takes less than
-	 *                        bits_per_pixel bits and is stored in the upper
-	 *                        bits of the pixel value
-	 *    var->{color}.length is set so that 1 << length is the number of available
-	 *                        palette entries
-	 *    cmap is not used
-	 *    RAMDAC[X] is programmed to (red, green, blue)
-	 *
-	 * Truecolor:
-	 *    does not use DAC. Usually 3 are present.
-	 *    var->{color}.offset contains start of bitfield
-	 *    var->{color}.length contains length of bitfield
-	 *    cmap is programmed to (red << red.offset) | (green << green.offset) |
-	 *                      (blue << blue.offset) | (transp << transp.offset)
-	 *    RAMDAC does not exist
-	 */
-#define CNVT_TOHW(val,width) ((((val)<<(width))+0x7FFF-(val))>>16)
-	switch (info->fix.visual) {
-	case FB_VISUAL_TRUECOLOR:
-	case FB_VISUAL_PSEUDOCOLOR:
-		red = CNVT_TOHW(red, info->var.red.length);
-		green = CNVT_TOHW(green, info->var.green.length);
-		blue = CNVT_TOHW(blue, info->var.blue.length);
-		transp = CNVT_TOHW(transp, info->var.transp.length);
-		break;
-	case FB_VISUAL_DIRECTCOLOR:
-		red = CNVT_TOHW(red, 8);	/* expect 8 bit DAC */
-		green = CNVT_TOHW(green, 8);
-		blue = CNVT_TOHW(blue, 8);
-		/* hey, there is bug in transp handling... */
-		transp = CNVT_TOHW(transp, 8);
-		break;
-	}
-#undef CNVT_TOHW
-	/* Truecolor has hardware independent palette */
-	if (info->fix.visual == FB_VISUAL_TRUECOLOR) {
-		u32 v;
-
-		if (regno >= 16)
-			return 1;
-
-		v = (red << info->var.red.offset) |
-		    (green << info->var.green.offset) |
-		    (blue << info->var.blue.offset) |
-		    (transp << info->var.transp.offset);
-		switch (info->var.bits_per_pixel) {
-		case 8:
-			break;
-		case 16:
-			((u32 *) (info->pseudo_palette))[regno] = v;
-			break;
-		case 24:
-		case 32:
-			((u32 *) (info->pseudo_palette))[regno] = v;
-			break;
-		}
-		return 0;
-	}
-	return 0;
-}
-
-    /*
-     *  Pan or Wrap the Display
-     *
-     *  This call looks only at xoffset, yoffset and the FB_VMODE_YWRAP flag
-     */
-
-static int vfb_pan_display(struct fb_var_screeninfo *var,
-			   struct fb_info *info)
-{
-	if (var->vmode & FB_VMODE_YWRAP) {
-		if (var->yoffset >= info->var.yres_virtual ||
-		    var->xoffset)
-			return -EINVAL;
-	} else {
-		if (var->xoffset + info->var.xres > info->var.xres_virtual ||
-		    var->yoffset + info->var.yres > info->var.yres_virtual)
-			return -EINVAL;
-	}
-	info->var.xoffset = var->xoffset;
-	info->var.yoffset = var->yoffset;
-	if (var->vmode & FB_VMODE_YWRAP)
-		info->var.vmode |= FB_VMODE_YWRAP;
-	else
-		info->var.vmode &= ~FB_VMODE_YWRAP;
-	return 0;
-}
-
-    /*
-     *  Most drivers don't need their own mmap function
-     */
-
-static int vfb_mmap(struct fb_info *info,
-		    struct vm_area_struct *vma)
-{
-	vma->vm_page_prot = pgprot_decrypted(vma->vm_page_prot);
-
-	return remap_vmalloc_range(vma, (void *)info->fix.smem_start, vma->vm_pgoff);
-}
-
-
-    /*
-     *  Initialisation
-     */
-
-static int vfb_probe(struct platform_device *dev)
-{
-	struct fb_info *info;
-	//unsigned int size = PAGE_ALIGN(videomemorysize);
-	int retval = -ENOMEM;
-
-	/*
-	 * For real video cards we use ioremap.
-	 */
-	/*if (!(videomemory = vmalloc_32_user(size)))
-		return retval;*/
-	videomemory = (void*)0x18000000; // Use a fixed address for the framebuffer for now
-
-	info = framebuffer_alloc(sizeof(u32) * 256, &dev->dev);
-	if (!info)
-		goto err;
-
-	info->flags |= FBINFO_VIRTFB;
-	info->screen_buffer = videomemory;
-	info->fbops = &vfb_ops;
-
-	if (!fb_find_mode(&info->var, info, mode_option,
-			  NULL, 0, &vfb_default, 8)){
-		fb_err(info, "Unable to find usable video mode.\n");
-		retval = -EINVAL;
-		goto err1;
-	}
-
-	vfb_fix.smem_start = (unsigned long) videomemory;
-	vfb_fix.smem_len = videomemorysize;
-	info->fix = vfb_fix;
-	info->pseudo_palette = info->par;
-	info->par = NULL;
-
-	retval = fb_alloc_cmap(&info->cmap, 256, 0);
-	if (retval < 0)
-		goto err1;
-
-	retval = register_framebuffer(info);
-	if (retval < 0)
-		goto err2;
-	platform_set_drvdata(dev, info);
-
-	vfb_set_par(info);
-
-	fb_info(info, "Virtual frame buffer device, using %ldK of video memory\n",
-		videomemorysize >> 10);
-	return 0;
-err2:
-	fb_dealloc_cmap(&info->cmap);
-err1:
-	framebuffer_release(info);
-err:
-	vfree(videomemory);
-	return retval;
-}
-
-static int vfb_remove(struct platform_device *dev)
-{
-	struct fb_info *info = platform_get_drvdata(dev);
-
-	if (info) {
-		unregister_framebuffer(info);
-		vfree(videomemory);
-		fb_dealloc_cmap(&info->cmap);
-		framebuffer_release(info);
-		return 0;
-	}
-	return -1;
-}
-
-static struct platform_driver vfb_driver = {
-	.probe	= vfb_probe,
-	.remove = vfb_remove,
-	.driver = {
-		.name	= "vfb",
-	},
-};
-
-static struct platform_device *vfb_device;
-
-static int __init vfb_init(void)
-{
-	int ret = 0;
-
-	if (!vfb_enable)
-		return -ENXIO;
-
-	ret = platform_driver_register(&vfb_driver);
-
-	if (!ret) {
-		vfb_device = platform_device_alloc("vfb", 0);
-
-		if (vfb_device)
-			ret = platform_device_add(vfb_device);
-		else
-			ret = -ENOMEM;
-
-		if (ret) {
-			platform_device_put(vfb_device);
-			platform_driver_unregister(&vfb_driver);
-		}
-	}
-
-	return ret;
-}
-
-module_init(vfb_init);
-
-static void __exit vfb_exit(void)
-{
-	platform_device_unregister(vfb_device);
-	platform_driver_unregister(&vfb_driver);
-}
-
-module_exit(vfb_exit);
-
-MODULE_DESCRIPTION("Frame Buffer driver for sandpiper");
 MODULE_LICENSE("GPL");
 MODULE_AUTHOR("Engin Cilasun");
+MODULE_DESCRIPTION("sandpiper framebuffer driver");
