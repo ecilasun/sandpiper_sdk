fd3bda61d8673d27a137821958a6cabf4f8bc324
Engin Cilasun <ecilasun@me.com>
2025-07-27 20:44:31 -0700

    more fixes

---
+++ scroll/scroll.c
@@ -0,0 +1,87 @@
+#include <stdint.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <signal.h>
+
+#include "core.h"
+#include "platform.h"
+#include "vpu.h"
+
+#define VIDEO_MODE      EVM_640_Wide
+#define VIDEO_COLOR     ECM_8bit_Indexed
+#define VIDEO_HEIGHT    480
+
+static struct EVideoContext s_vctx;
+static struct EVideoSwapContext s_sctx;
+struct SPSizeAlloc frameBufferA;
+struct SPSizeAlloc frameBufferB;
+static struct SPPlatform s_platform;
+
+void shutdowncleanup()
+{
+	// Reset shifts
+	VPUShiftCache(&s_vctx, 0);
+	VPUShiftScanout(&s_vctx, 0);
+	VPUShiftPixel(&s_vctx, 0);
+
+	// Switch to fbcon buffer
+	VPUSetScanoutAddress(&s_vctx, 0x18000000);
+	VPUSetVideoMode(&s_vctx, EVM_640_Wide, ECM_16bit_RGB, EVS_Enable);
+
+	// Yield physical memory and reset video routines
+	VPUShutdownVideo();
+
+	// Release allocations
+	SPFreeBuffer(&s_platform, &frameBufferB);
+	SPFreeBuffer(&s_platform, &frameBufferA);
+
+	// Shutdown platform
+	SPShutdownPlatform(&s_platform);
+}
+
+void sigint_handler(int s)
+{
+	shutdowncleanup();
+	exit(0);
+}
+
+int main(int argc, char** argv)
+{
+	SPInitPlatform(&s_platform);
+	VPUInitVideo(&s_vctx, &s_platform);
+	uint32_t stride = VPUGetStride(VIDEO_MODE, VIDEO_COLOR);
+	frameBuffer.size = stride*VIDEO_HEIGHT;
+	SPAllocateBuffer(&s_platform, &frameBuffer);
+
+	atexit(shutdowncleanup);
+	signal(SIGINT, &sigint_handler);
+	signal(SIGTERM, &sigint_handler);
+
+	// Set aside space for the decompressed image
+	// NOTE: Video scanout buffer has to be aligned at 64 byte boundary
+	image = (uint16_t*)frameBuffer.cpuAddress;
+
+	VPUSetWriteAddress(&s_vctx, (uint32_t)frameBuffer.cpuAddress);
+	VPUSetScanoutAddress(&s_vctx, (uint32_t)frameBuffer.dmaAddress);
+	VPUSetDefaultPalette(&s_vctx);
+	VPUSetVideoMode(&s_vctx, VIDEO_MODE, VIDEO_COLOR, EVS_Enable);
+
+	VPUShiftCache(&s_vctx, 0);
+	VPUShiftScanout(&s_vctx, 0);
+	VPUShiftPixel(&s_vctx, 0);
+
+	do
+	{
+		VPUConsoleResolve(&s_vctx);
+
+		if (s_sctx.cycle % 15 == 0)
+		{
+			// Every 15 frames, do something
+		}
+
+		VPUWaitVSync(&s_vctx); // This and other reads from VPU cause a hardware freeze, figure out why
+		VPUSwapPages(&s_vctx, &s_sctx);
+	} while(1);
+
+	return 0;
+}
