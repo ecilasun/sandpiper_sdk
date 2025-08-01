27dd7ff3cdd09c85610a25dc0ba730d4cd875c3b
Engin Cilasun <ecilasun@me.com>
2025-08-01 16:27:19 -0700

    Adding more samples

---
+++ samples/doom/src/riscv/i_system.c
@@ -0,0 +1,207 @@
+/*
+ * i_system.c
+ *
+ * System support code
+ *
+ * Copyright (C) 1993-1996 by id Software, Inc.
+ * Copyright (C) 2021 Sylvain Munaut
+ * All rights reserved.
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ */
+
+
+#include <stdarg.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#include "doomdef.h"
+#include "doomstat.h"
+
+#include "d_main.h"
+#include "g_game.h"
+#include "m_misc.h"
+#include "i_sound.h"
+#include "i_video.h"
+
+#include "i_system.h"
+
+#include "basesystem.h"
+#include "core.h"
+#include "serialinringbuffer.h"
+#include "task.h"
+#include "keyboard.h"
+#include "joystick.h"
+
+void
+I_Init(void)
+{
+	I_InitSound();
+
+	struct STaskContext* taskctx = TaskGetContext(0);
+}
+
+
+byte *
+I_ZoneBase(int *size)
+{
+	/* Give 8M to DOOM */
+	*size = 8 * 1024 * 1024;
+	return (byte *) malloc (*size);
+}
+
+// returns time in 1/TICRATEth second tics
+int
+I_GetTime (void)
+{
+	return (ClockToMs(E32ReadTime())*TICRATE)/1000;
+}
+
+static void
+I_GetRemoteEvent(void)
+{
+	static uint32_t oldcountKey = 0xAAAABBBB;
+	static uint32_t oldcountJoystick = 0xAAAABBBB;
+
+	volatile struct SKeyboardState* keyState = KeyboardGetState();
+	volatile struct SJoystickState* joystickState = JoystickGetState();
+
+	if (oldcountKey != keyState->count && keyState->scancode != 0x0)
+	{
+		oldcountKey = keyState->count;
+
+		uint8_t scancode = keyState->scancode;
+
+		event_t event;
+		// See keyboard scan codes for more info
+		switch(scancode)
+		{
+			case 88:	{ event.data1 = KEY_ENTER; break; }
+			case 40:	{ event.data1 = KEY_ENTER; break; }
+			case 79:	{ event.data1 = KEY_RIGHTARROW; break; }
+			case 80:	{ event.data1 = KEY_LEFTARROW; break; }
+			case 81:	{ event.data1 = KEY_DOWNARROW; break; }
+			case 82:	{ event.data1 = KEY_UPARROW; break; }
+			case 45: 	{ event.data1 = KEY_MINUS; break; }
+			case 46:	{ event.data1 = KEY_EQUALS; break; }
+			case 41:	{ event.data1 = KEY_ESCAPE; break; }
+			case 43:	{ event.data1 = KEY_TAB; break; }
+			case 42:	{ event.data1 = KEY_BACKSPACE; break; }
+			case 229:	{ event.data1 = KEY_RSHIFT; break; }
+			case 228:	{ event.data1 = KEY_RCTRL; break; }
+			case 230:	{ event.data1 = KEY_RALT; break; }
+			case 226:	{ event.data1 = KEY_LALT; break; }
+			case 72:	{ event.data1 = KEY_PAUSE; break; }
+			case 58:	{ event.data1 = KEY_F1; break; }
+			case 59:	{ event.data1 = KEY_F2; break; }
+			case 60:	{ event.data1 = KEY_F3; break; }
+			case 61:	{ event.data1 = KEY_F4; break; }
+			case 62:	{ event.data1 = KEY_F5; break; }
+			case 63:	{ event.data1 = KEY_F6; break; }
+			case 64:	{ event.data1 = KEY_F7; break; }
+			case 65:	{ event.data1 = KEY_F8; break; }
+			case 66:	{ event.data1 = KEY_F9; break; }
+			case 67:	{ event.data1 = KEY_F10; break; }
+			case 68:	{ event.data1 = KEY_F11; break; }
+			case 69:	{ event.data1 = KEY_F12; break; }
+			default:	{ event.data1 = KeyboardScanCodeToASCII(scancode, 0); break; }
+		}
+		event.type = keyState->state&1 ? ev_keydown : ev_keyup;
+		D_PostEvent(&event);
+	}
+
+	if (oldcountJoystick != joystickState->count)
+	{
+		event_t event;
+		event.type = ev_joystick;
+		event.data1 = joystickState->buttons;
+		event.data2 = (int)joystickState->axis[0];
+		event.data3 = (int)joystickState->axis[1];
+		D_PostEvent(&event);
+	}
+}
+
+void
+I_StartFrame(void)
+{
+	/* Nothing to do */
+}
+
+void
+I_StartTic(void)
+{
+	I_GetRemoteEvent();
+}
+
+ticcmd_t *
+I_BaseTiccmd(void)
+{
+	static ticcmd_t emptycmd;
+	return &emptycmd;
+}
+
+
+void
+I_Quit(void)
+{
+	D_QuitNetGame();
+	I_ShutdownSound();
+	M_SaveDefaults();
+	I_ShutdownGraphics();
+	exit(0); // NOTE: The environment we're going to return to has been destroyed
+}
+
+
+byte *
+I_AllocLow(int length)
+{
+	byte*	mem;
+	mem = (byte *)malloc (length);
+	memset (mem,0,length);
+	return mem;
+}
+
+
+void
+I_Tactile
+( int on,
+  int off,
+  int total )
+{
+	// UNUSED.
+	on = off = total = 0;
+}
+
+
+void
+I_Error(char *error, ...)
+{
+	va_list	argptr;
+
+	// Message first.
+	va_start (argptr,error);
+	printf ("Error: ");
+	vprintf (error, argptr);
+	printf ("\n");
+	va_end (argptr);
+
+	fflush( stdout );
+
+	// Shutdown. Here might be other errors.
+	if (demorecording)
+		G_CheckDemoStatus();
+
+	D_QuitNetGame ();
+	I_ShutdownGraphics();
+
+	exit(-1);
+}
---
+++ samples/doom/src/riscv/i_video.c
@@ -0,0 +1,101 @@
+/*
+ * i_video.c
+ *
+ * Video system support code
+ *
+ * Copyright (C) 2021 Sylvain Munaut
+ * All rights reserved.
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ */
+
+#include <stdint.h>
+#include <string.h>
+#include <stdlib.h>
+
+#include "../doomdef.h"
+
+#include "../i_system.h"
+#include "../v_video.h"
+#include "../i_video.h"
+
+#include "basesystem.h"
+#include "core.h"
+#include "vpu.h"
+
+uint8_t *framebuffer;
+struct EVideoContext g_vctx;
+
+void
+I_InitGraphics(void)
+{
+	usegamma = 1;
+
+	// Allocate 40 pixel more since out video output is 240 vs the default 200 pixels here
+	framebuffer = VPUAllocateBuffer(SCREENWIDTH*(SCREENHEIGHT+40));
+	memset(framebuffer, 0x0, SCREENWIDTH*(SCREENHEIGHT+40));
+
+	g_vctx.m_cmode = ECM_8bit_Indexed;
+	g_vctx.m_vmode = EVM_320_Wide;
+	VPUSetVMode(&g_vctx, EVS_Enable);
+	VPUSetWriteAddress(&g_vctx, (uint32_t)framebuffer);
+	VPUSetScanoutAddress(&g_vctx, (uint32_t)framebuffer);
+}
+
+void
+I_ShutdownGraphics(void)
+{
+	VPUSetVMode(&g_vctx, EVS_Disable);
+}
+
+void
+I_SetPalette(byte* palette)
+{
+	// Copy palette to G-RAM
+	byte r, g, b;
+	for (int i=0 ; i<256 ; i++) {
+		r = gammatable[usegamma][*palette++]>>4;
+		g = gammatable[usegamma][*palette++]>>4;
+		b = gammatable[usegamma][*palette++]>>4;
+		VPUSetPal(i, r, g, b);
+	}
+}
+
+
+void
+I_UpdateNoBlit(void)
+{
+	// hmm....
+}
+
+void
+I_FinishUpdate (void)
+{
+	// Copy screen to framebuffer
+	memcpy(framebuffer, screens[0], SCREENWIDTH*SCREENHEIGHT);
+	// Write pending data to memory to ensure all writes are visible to scan-out
+	CFLUSH_D_L1();
+}
+
+
+void
+I_WaitVBL(int count)
+{
+	// Wait until we exit current frame's vbcounter and enter the next one
+	VPUWaitVSync();
+}
+
+void
+I_ReadScreen(byte* scr)
+{
+	// Copy what's on screen
+	memcpy (scr, screens[0], SCREENWIDTH*SCREENHEIGHT);
+}
