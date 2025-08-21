d447081a178c2762a5b3f94d8014fc932cf2a763
Engin Cilasun <ecilasun@me.com>
2025-08-20 17:46:55 -0700

    multithreading mandelbrot

--- samples/mandelbrot/mandelbrot.cpp
+++ samples/mandelbrot/mandelbrot.cpp
@@ -12,13 +12,35 @@
 #include <math.h>
 #include <cmath>
 #include <signal.h>
+#include <pthread.h>
 
 #include "core.h"
 #include "platform.h"
 #include "vpu.h"
 
+const float X = -0.235125f;
+const float Y = 0.827215f;
+
 static struct SPPlatform* s_platform = NULL;
 struct SPSizeAlloc framebuffer;
+
+struct SThreadData
+{
+	int tid;
+	int tilex;
+	int tiley;
+	int go;
+	int running;
+};
+
+void InitThreadData(SThreadData* data, int tid, int tilex, int tiley)
+{
+	data->tid = tid;
+	data->tilex = tilex;
+	data->tiley = tiley;
+	data->go = 0;
+	data->running = 0;
+}
 
 float evalMandel(const int maxiter, int col, int row, float ox, float oy, float sx)
 {
@@ -40,16 +62,13 @@ float evalMandel(const int maxiter, int col, int row, float ox, float oy, float 
 	return iteration;
 }
 
-int tilex = 0;
-int tiley = 0;
-
-void mandelbrotFloat(float ox, float oy, float sx)
+void mandelbrotFloat(float ox, float oy, float sx, int tilex, int tiley)
 {
 	uint32_t stride = VPUGetStride(EVM_320_Wide, ECM_16bit_RGB);
 	uint16_t* framebuffer = (uint16_t*)s_platform->sc->writepage;
 
 	// http://blog.recursiveprocess.com/2014/04/05/mandelbrot-fractal-v2/
-	float R = 27.71f-5.156f*logf(sx);
+	float ratio = 27.71f-5.156f*logf(sx);
 
 	for (int y = 0; y < 16; ++y)
 	{
@@ -58,9 +77,9 @@ void mandelbrotFloat(float ox, float oy, float sx)
 		{
 			int col = x + tilex*16;
 
-			float M = evalMandel(R, col, row, ox, oy, sx);
-			float ratio = M / R;
-			int c = int(ratio*31.f);
+			float M = evalMandel(ratio, col, row, ox, oy, sx);
+			float local_ratio = M / ratio;
+			int c = int(local_ratio*31.f);
 			framebuffer[col + (row*stride>>1)] = MAKECOLORRGB16(c, c, c);
 		}
 	}
@@ -69,6 +88,43 @@ void mandelbrotFloat(float ox, float oy, float sx)
 	// d(c) = |Z|·log|Z|/|Z'|
 	// float d = 0.5*sqrt(dot(z,z)/dot(dz,dz))*log(dot(z,z));
 	// if( di>0.5 ) d=0.0;
+}
+
+void* mandelbrot(void* arg)
+{
+	SThreadData* data = (SThreadData*)arg;
+	int tid = data->tid;
+	int tilex = data->tilex;
+	int tiley = data->tiley;
+
+	while(1)
+	{
+		if (data->go)
+		{
+			data->go = 0;
+			data->running = 1;
+			mandelbrotFloat(X, Y, R, tilex, tiley);
+			data->running = 0;
+		}
+	}
+
+	return NULL;
+}
+
+void PickNextTile(int* tilex, int* tiley, float* R)
+{
+	(*tilex)++;
+	if (*tilex == 20)
+	{
+		*tilex = 0;
+		(*tiley)++;
+	}
+	if (*tiley == 15)
+	{
+		*tiley = 0;
+		// Zoom
+		*R += 0.0002f;
+	}
 }
 
 int main()
@@ -95,27 +151,34 @@ int main()
 	VPUSwapPages(s_platform->vx, s_platform->sc);
 	VPUClear(s_platform->vx, 0x00000000);
 
-	float R = 4.0E-6f + 0.01f; // Step once to see some detail due to adaptive code
-	float X = -0.235125f;
-	float Y = 0.827215f;
+	SThreadData threadData1, threadData2;
+	pthread_t thread1, thread2;
+	InitThreadData(&threadData1, 0, 0, 0);
+	InitThreadData(&threadData2, 1, 0, 0);
+	int success = pthread_create(&thread1, NULL, mandelbrot, &threadData1);
+	success = pthread_create(&thread2, NULL, mandelbrot, &threadData2);
+	pthread_join(thread1, NULL);
+	pthread_join(thread2, NULL);
 
+	int tilex = 0;
+	int tiley = 0;
+	float R = 4.0E-6f + 0.01f;
 	while(1)
 	{
-		// Generate one line of mandelbrot into offscreen buffer
-		// NOTE: It is unlikely that CPU write speeds can catch up with VPU transfer speed, should not see any flicker
-		mandelbrotFloat(X,Y,R);
-
-		tilex++;
-		if (tilex == 20)
+		if (threadData1.running == 0)
 		{
-			tilex = 0;
-			tiley++;
+			PickNextTile(&tilex, &tiley, &R);
+			threadData1.tilex = tilex;
+			threadData1.tiley = tiley;
+			threadData1.go = 1;
 		}
-		if (tiley == 15)
+
+		if (threadData2.running == 0)
 		{
-			tiley = 0;
-			// Zoom
-			R += 0.0002f;
+			PickNextTile(&tilex, &tiley, &R);
+			threadData2.tilex = tilex;
+			threadData2.tiley = tiley;
+			threadData2.go = 1;
 		}
 	}
 
