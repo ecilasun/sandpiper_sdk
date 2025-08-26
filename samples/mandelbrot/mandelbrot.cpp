5e512e24c333ff122ca4d77859ac6fb2e32db59f
Engin Cilasun <ecilasun@me.com>
2025-08-26 01:00:30 +0000

    Updated mandelbrot as a multithreading sample

--- samples/mandelbrot/mandelbrot.cpp
+++ samples/mandelbrot/mandelbrot.cpp
@@ -1,8 +1,11 @@
 /** \file
- * Mandelbrot set example.
+ * Multithreaded Mandelbrot set example.
  *
  * \ingroup examples
  * This example demonstrates the use of the VPU frame buffers to render a Mandelbrot set.
+ * The render work is split across the two CPUs, where upon task completion, each CPU
+ * pulls the next available tile to render.
+ * It's a very simple task system, but should suffice to demontstrate basic thread usage.
  */
 
 #include <inttypes.h>
@@ -22,9 +25,6 @@
 const float X = -0.235125f;
 const float Y = 0.827215f;
 
-static struct SPPlatform* s_platform = NULL;
-struct SPSizeAlloc framebuffer;
-
 struct SThreadData
 {
 	int tid;
@@ -32,17 +32,19 @@ struct SThreadData
 	int tiley;
 	float R;
 	std::atomic<bool> go;
-	std::atomic<bool> running;
+	struct SPPlatform* platform = nullptr;
+	struct SPSizeAlloc* framebuffer = nullptr;
 };
 
-void InitThreadData(SThreadData* data, int tid, float R, int tilex, int tiley)
+void InitThreadData(SThreadData* data, struct SPPlatform* platform, struct SPSizeAlloc* framebuffer, int tid, float R, int tilex, int tiley)
 {
+	data->platform = platform;
+	data->framebuffer = framebuffer;
 	data->tid = tid;
 	data->tilex = tilex;
 	data->tiley = tiley;
 	data->R = R;
 	data->go.store(false);
-	data->running.store(false);
 }
 
 float evalMandel(const int maxiter, int col, int row, float ox, float oy, float sx)
@@ -65,11 +67,8 @@ float evalMandel(const int maxiter, int col, int row, float ox, float oy, float 
 	return iteration;
 }
 
-void mandelbrotFloat(float ox, float oy, float sx, int tilex, int tiley)
+void mandelbrotFloat(uint32_t tid, uint32_t stride, uint16_t* frame, float ox, float oy, float sx, int tilex, int tiley)
 {
-	uint32_t stride = VPUGetStride(EVM_320_Wide, ECM_16bit_RGB);
-	uint16_t* framebuffer = (uint16_t*)s_platform->sc->writepage;
-
 	// http://blog.recursiveprocess.com/2014/04/05/mandelbrot-fractal-v2/
 	float ratio = 27.71f-5.156f*logf(sx);
 
@@ -83,7 +82,8 @@ void mandelbrotFloat(float ox, float oy, float sx, int tilex, int tiley)
 			float M = evalMandel(ratio, col, row, ox, oy, sx);
 			float local_ratio = M / ratio;
 			int c = int(local_ratio*31.f);
-			framebuffer[col + (row*stride>>1)] = MAKECOLORRGB16(c, c, c);
+			//c = tid == 0 ? c : c | 2; // DEBUG: watermark threads
+			frame[col + (row*stride>>1)] = MAKECOLORRGB16(c, c, c);
 		}
 	}
 
@@ -97,17 +97,18 @@ void* mandelbrot(void* arg)
 {
 	SThreadData* data = (SThreadData*)arg;
 
+	uint32_t stride = VPUGetStride(EVM_320_Wide, ECM_16bit_RGB);
+
 	while(1)
 	{
-		if (data->go.load())
+		if (data->go.load() == true)
 		{
-			printf(">%d\n", data->tid);
+			//printf(">%d\n", data->tid);
 			int tilex = data->tilex;
 			int tiley = data->tiley;
 			float R = data->R;
+			mandelbrotFloat(data->tid, stride, (uint16_t*)data->platform->sc->writepage, X, Y, R, tilex, tiley);
 			data->go.store(false);
-			mandelbrotFloat(X, Y, R, tilex, tiley);
-			data->running.store(false);
 		}
 
 		sched_yield();
@@ -135,102 +136,92 @@ void PickNextTile(int* tilex, int* tiley, float* R)
 int main()
 {
 	// Initialize platform and video system
-	s_platform = SPInitPlatform();
-	if (!s_platform)
+	SPPlatform* platform = new SPPlatform();
+	platform = SPInitPlatform();
+	if (!platform)
 	{
 		printf("Failed to initialize platform\n");
 		return -1;
 	}
-	VPUInitVideo(s_platform->vx, s_platform);
+	VPUInitVideo(platform->vx, platform);
 
 	// Grab video buffer
 	uint32_t stride = VPUGetStride(EVM_320_Wide, ECM_16bit_RGB);
-	framebuffer.size = stride*240;
-	SPAllocateBuffer(s_platform, &framebuffer);
+	SPSizeAlloc* framebuffer = new SPSizeAlloc();
+	framebuffer->size = stride*240;
+	SPAllocateBuffer(platform, framebuffer);
 
 	// Set up the video mode and frame pointers
-	VPUSetVideoMode(s_platform->vx, EVM_320_Wide, ECM_16bit_RGB, EVS_Enable);
-	s_platform->sc->cycle = 0;
-	s_platform->sc->framebufferA = &framebuffer; // Not double-buffering
-	s_platform->sc->framebufferB = &framebuffer;
-	VPUSwapPages(s_platform->vx, s_platform->sc);
-	VPUClear(s_platform->vx, 0x00000000);
+	VPUSetVideoMode(platform->vx, EVM_320_Wide, ECM_16bit_RGB, EVS_Enable);
+	platform->sc->cycle = 0;
+	platform->sc->framebufferA = framebuffer; // Not double-buffering
+	platform->sc->framebufferB = framebuffer;
+	VPUSwapPages(platform->vx, platform->sc);
+	VPUClear(platform->vx, 0x00000000);
 
 	float R = 4.0E-6f + 0.01f;
 
-	//SThreadData *threadData1;
-	//SThreadData *threadData2;
-	//pthread_t thread1;
-	//pthread_t thread2;
-	//pthread_attr_t attr1;
-	//pthread_attr_t attr2;
-	//cpu_set_t cpuset1;
-	//cpu_set_t cpuset2;
+	SThreadData *threadData1;
+	SThreadData *threadData2;
+	pthread_t thread1;
+	pthread_t thread2;
+	pthread_attr_t attr1;
+	pthread_attr_t attr2;
+	cpu_set_t cpuset1;
+	cpu_set_t cpuset2;
 
-	//threadData1 = new SThreadData();
-	//threadData2 = new SThreadData();
-	//InitThreadData(threadData1, 1, R, 0, 0);
-	//InitThreadData(threadData2, 2, R, 0, 0);
+	threadData1 = new SThreadData();
+	threadData2 = new SThreadData();
+	InitThreadData(threadData1, platform, framebuffer, 0, R, 0, 0);
+	InitThreadData(threadData2, platform, framebuffer, 1, R, 0, 0);
 
-	//CPU_ZERO(&cpuset1);
-	//CPU_ZERO(&cpuset2);
-	//CPU_SET(1, &cpuset1);
-	//CPU_SET(2, &cpuset2);
+	CPU_ZERO(&cpuset1);
+	CPU_ZERO(&cpuset2);
+	CPU_SET(0, &cpuset1); // Only on CPU#0
+	CPU_SET(1, &cpuset2); // Only on CPU#1
 
-	// pthread_attr_init(&attr1);
-	// pthread_attr_setaffinity_np(&attr1, sizeof(cpu_set_t), &cpuset1);
-	// pthread_attr_setdetachstate(&attr1, PTHREAD_CREATE_JOINABLE);
+	pthread_attr_init(&attr1);
+	pthread_attr_setaffinity_np(&attr1, sizeof(cpu_set_t), &cpuset1);
+	pthread_attr_setdetachstate(&attr1, PTHREAD_CREATE_DETACHED);
 
-	// pthread_attr_init(&attr2);
-	// pthread_attr_setaffinity_np(&attr2, sizeof(cpu_set_t), &cpuset2);
-	// pthread_attr_setdetachstate(&attr2, PTHREAD_CREATE_JOINABLE);
+	pthread_attr_init(&attr2);
+	pthread_attr_setaffinity_np(&attr2, sizeof(cpu_set_t), &cpuset2);
+	pthread_attr_setdetachstate(&attr2, PTHREAD_CREATE_DETACHED);
 
 	// Create threads
-	//int success = pthread_create(&thread1, &attr1, mandelbrot, (void*)threadData1);
-	//if (success != 0) {
-	//	printf("Failed to create thread 1\n");
-	//	return -1;
-	//}
+	int success = pthread_create(&thread1, &attr1, mandelbrot, (void*)threadData1);
+	if (success != 0) {
+		printf("Failed to create thread 1\n");
+		return -1;
+	}
 
-	//success = pthread_create(&thread2, &attr2, mandelbrot, (void*)threadData2);
-	//if (success != 0) {
-	//	printf("Failed to create thread 2\n");
-	//	return -1;
-	//}
-
-	//pthread_join(thread1, NULL);
-	//pthread_join(thread2, NULL);
+	success = pthread_create(&thread2, &attr2, mandelbrot, (void*)threadData2);
+	if (success != 0) {
+		printf("Failed to create thread 2\n");
+		return -1;
+	}
 
 	int tilex = 0;
 	int tiley = 0;
 	while(1)
 	{
-		PickNextTile(&tilex, &tiley, &R);
-
-//		if ((tilex+tiley*21)%2==0) -> CPU0 else CPU1
-
-		mandelbrotFloat(X, Y, R, tilex, tiley);
-
-		/*if (threadData1->running.load() == false)
+		if (threadData1->go.load() == false)
 		{
-			printf("<%d\n", threadData1->tid);
-			threadData1->running.store(true);
 			PickNextTile(&tilex, &tiley, &R);
 			threadData1->tilex = tilex;
 			threadData1->tiley = tiley;
 			threadData1->R = R;
 			threadData1->go.store(true);
-		}*/
+		}
 
-		/*if (threadData2->running.load() == false)
+		if (threadData2->go.load() == false)
 		{
-			threadData2->running.store(true);
 			PickNextTile(&tilex, &tiley, &R);
 			threadData2->tilex = tilex;
 			threadData2->tiley = tiley;
 			threadData2->R = R;
 			threadData2->go.store(true);
-		}*/
+		}
 
 		sched_yield();
 	}
