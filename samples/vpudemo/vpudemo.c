35cf724d4e49ef0e1f3ca3fba6f3bec754ea1323
Engin Cilasun <ecilasun@me.com>
2025-09-19 19:36:16 +0000

    removed color increment to leave it up to the VCP for demo

--- samples/vpudemo/vpudemo.c
+++ samples/vpudemo/vpudemo.c
@@ -25,17 +25,17 @@ struct SPSizeAlloc frameBufferB;
 // at start of every scanline by waiting for pixel 0
 // The program clocks at approximately one instruction per pixel
 static uint32_t s_vcpprogram[] = {
-	vcp_setacc(0xFFFFFF),				// White color in ACC (Accumulator Register is R0)
-	vcp_copyreg(0x01, 0x00),			// Copy ACC to R1
-	vcp_setacc(0x000001),				// Increment value in ACC
-	vcp_copyreg(0x02, 0x00),			// Copy ACC to R2
+	vcp_setacc(0xFFFFFF),			// White color in ACC (Accumulator Register is R0)
+	vcp_copyreg(0x01, 0x00),		// Copy ACC to R1
+	vcp_setacc(0x000001),			// Increment value in ACC
+	vcp_copyreg(0x02, 0x00),		// Copy ACC to R2
 // loop:
-	vcp_waitcolumn(0x00),				// Wait for pixel 0 of the current scanline
-	vcp_setpal(0x00, 0x01),				// Set PAL[0] to R1
-	vcp_add(0x01, 0x02),				// Increment R1 by R2
+	vcp_waitcolumn(0x00),			// Wait for pixel 0 of the current scanline
+	vcp_setpal(0x00, 0x01),			// Set PAL[0] to R1
+	vcp_add(0x01, 0x02),			// Increment R1 by R2
 	vcp_compare(0x00, 0x00, COND_EQ),	// Set branch condition to 'R0==R0' in ACC (i.e. COND_ALWAYS)
-	vcp_branch(0x10),					// Unconditional branch to byte 16 (start of loop) based on ACC
-	vcp_halt(),							// Good practice to pad with a halt instruction
+	vcp_branch(0x10),			// Unconditional branch to byte 16 (start of loop) based on ACC
+	vcp_halt(),				// Good practice to pad with a halt instruction
 };
 
 int main(int argc, char** argv)
@@ -101,7 +101,7 @@ int main(int argc, char** argv)
 
 		// VPU program demo goes here
 		VPUClear(s_platform->vx, color);
-		color++;
+		//color++;
 
 		// Queue vsync
 		// This will be processed by the VPU asynchronously when the video beam reaches the vertical blanking interval (vblank).
