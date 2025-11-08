c6d85e772e0d90c6675453bc5e339e1b71a3f63a
Engin Cilasun <ecilasun@me.com>
2025-11-08 16:48:37 +0000

    sample changes

--- samples/vcpdemo/vcpdemo.c
+++ samples/vcpdemo/vcpdemo.c
@@ -41,15 +41,15 @@ static const char* states[] = {
 // Given we run at approximately 55.5MIPS, we can afford to run a program that executes around 925925 instructions per frame.
 // This is plenty for simple effects like changing palette entries on scanline boundaries, simple raster effects, etc.
 
-// Tiny program to change palette color 0
-// at start of every scanline by waiting for pixel 0
+// Tiny program to change some palette colors at scanline zero
 static uint32_t s_vcpprogram[] = {
-	vcp_ldim(0x01, 0x276631),	// Load a color into R1
+	vcp_ldim(0x01, 0x550000),	// Load a color into R1
 	vcp_ldim(0x02, 0x000005),	// Load 5 into R2 (increment)
 	vcp_ldim(0x03, 0x000002),	// Load 2 into R3 (for PAL[2])
 	vcp_ldim(0x04, 0x000010),	// Load 16 into R4 (offset of loop:)
 // loop:
 	vcp_wscn(0x00),			// Wait for scanline zero (R0==0)
+	vcp_wpix(0x00),			// Wait for pixel zero (R0==0)
 	vcp_pwrt(0x00, 0x01),		// Set PAL[0] to R1 (R0==0)
 	vcp_pwrt(0x03, 0x01),		// Set PAL[2] to R1 (R3==2)
 	vcp_radd(0x01, 0x01, 0x02),	// R1 = R1 + R2(5)
@@ -140,27 +140,25 @@ int main(int argc, char** argv)
 	printf("PC:%X R:%s C:%d E:%X F:%d\n", (stat>>9), states[(stat>>6)&7], (stat>>5)&1, (stat>>1)&0xF, stat&1);
 
 	printf("Entering demo...\n");
-	uint32_t color = 0x00040201; // RED(x04) entryshould change by the program
-	uint32_t frame = 0;
+	uint32_t color = 0x04030201; // VCP program updates some of these colors
 	do
 	{
 		// Vsync barrier
 		// Wait for previous frame to finish and swap buffers
 		while(VPUGetFIFONotEmpty(s_platform->vx)) { }
 		VPUSwapPages(s_platform->vx, s_platform->sc);
-		frame++;
 
 		// VPU program demo goes here
 		VPUClear(s_platform->vx, color);
-		if (frame%10 == 0)
-			color = (color<<8) | ((color&0xFF000000)>>24); // roll
+		color = (color<<8) | ((color&0xFF000000)>>24); // roll
 
+		// Show program debug info
 		uint32_t* wordA = (uint32_t*)s_platform->sc->writepage;
-		for (int i=0;i<512;++i)
+		for (int i=0;i<128;++i)
 		{
 			stat = VCPStatus(s_platform);
 			*wordA = stat;
-			wordA++;
+			wordA+=2;
 		}
 
 		// Queue vsync
