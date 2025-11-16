3e08693b01dc960dddb059bf7efab1801c9d041e
Engin Cilasun <ecilasun@me.com>
2025-11-16 01:08:07 -0800

    added new instructions

--- SDK/vcp.h
+++ SDK/vcp.h
@@ -19,6 +19,11 @@
 #define VCP_BRANCH			0x08
 #define VCP_STORE			0x09
 #define VCP_LOAD			0x0A
+#define VCP_READSCANLINE	0x0B
+#define VCP_READSCANPIXEL	0x0C
+#define VCP_AND				0x0D
+#define VCP_OR				0x0E
+#define VCP_XOR				0x0F
 
 #define DESTREG(reg)			((reg & 0xF) << 4)
 #define SRCREG1(reg)			((reg & 0xF) << 8)
@@ -51,6 +56,11 @@
 #define vcp_branch(address, src)			(	0					| SRCREG2(src)		| SRCREG1(addrs)	| 0					| VCP_BRANCH		)
 #define vcp_store(addrs, src)				(	0					| SRCREG2(src)		| SRCREG1(addrs)	| 0					| VCP_STORE			)
 #define vcp_load(addrs, dest)				(	0					| 0					| SRCREG1(addrs)	| DESTREG(dest)		| VCP_LOAD			)
+#define vcp_scanline_read(dest)				(	0					| 0					| 0					| DESTREG(dest)		| VCP_READSCANLINE	)
+#define vcp_scanpixel_read(dest)			(	0					| 0					| 0					| DESTREG(dest)		| VCP_READSCANPIXEL	)
+#define vcp_and(dest, src1, src2)			(	0					| SRCREG2(src2)		| SRCREG1(src1)		| DESTREG(dest)		| VCP_AND			)
+#define vcp_or(dest, src1, src2)			(	0					| SRCREG2(src2)		| SRCREG1(src1)		| DESTREG(dest)		| VCP_OR			)
+#define vcp_xor(dest, src1, src2)			(	0					| SRCREG2(src2)		| SRCREG1(src1)		| DESTREG(dest)		| VCP_XOR			)
 
 void VCPUploadProgram(struct SPPlatform *ctx, const uint32_t* _program, enum EVCPBufferSize size);
 void VCPExecProgram(struct SPPlatform *ctx, const uint8_t _execFlags);
